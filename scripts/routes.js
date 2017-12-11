9ad64ca703c596cbf9abbb5266d0aed3d0817108
missalissa <alissa.beddow@gmail.com>
2017-12-10 19:39:09 -0800

    added routes.js

--- scripts/routes.js
+++ scripts/routes.js
@@ -5,6 +5,10 @@ page('/', (ctx, next) => {
 
 // page('/books/:id', app.Book.fetchOne, app.bookView.initDetailPage); // passing in the function to be called later, not the value of the funtion
 
+// TODO: Build a route handler for /new that will a) show a form that takes data and b) on submit the form and add data to our database
+// page('/new', app.newView.initNewPage);
+
+// TODO: Write app.newVIew.initNewPage; a) hide all other sections, b) show a section with a form, and c) listen for submit event, which will create a new book in the database (via a POST request) will need body parser to access form data
 page('/about', aboutView.initAboutPage); // passing in the function to be called later, not the value of the funtion
 
 page('*', (ctx, next) => { console.log('Nope, nothing here'); }); // signature of the callback function for the page function
